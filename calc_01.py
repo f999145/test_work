8245e0f5151c12e0732447ee8dac199b03404ee7
f999145 <f999145@yandex.ru>
2022-11-15 23:43:40 +0300

    update

--- calc_01.py
+++ calc_01.py
@@ -1,21 +1,8 @@
-price = 6
-num_01 = 12458.771
-num_02 = 12460.232
-num_03 = 12473.320
-d_01 = 26
-d_02 = 27
-d_03 = 30+4
-#test
-del_num = num_02-num_01
-score_month_01 = (num_02-num_01)/(d_02-d_01)
-score_month_02 = (num_03-num_02)/(d_03-d_02)
-
-print(f'За 27 инюня потрачено {round(num_02-num_01,1)} м3')
-print('Тогда траты за месяц')
-print(f'        {round(score_month_01*30,1)} м3 газа')
-print(f'        {int(score_month_01*30*price)} руб/м')
-print()
-print(f'С 27 июня по 4 июля потрачено {round(num_03-num_02,1)} м3')
-print('Тогда траты за месяц')
-print(f'        {round(score_month_02*30,1)} м3 газа')
-print(f'        {int(score_month_02*30*price)} руб/м')
+import math
+S_0 = 1.5
+S_1 = 2.5
+pi_0 = math.pi
+d_0 = 2*math.sqrt(S_0/pi_0)
+d_1 = 2*math.sqrt(S_1/pi_0)
+print('диаметр 1,5 квадрата равен ', round(d_0, 2), ' мм')
+print('диаметр 2,5 квадрата равен ', round(d_1, 2), ' мм')
